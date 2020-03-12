337313fc809e3c912623dcfcdaf3db4304266272
allan <acardoso@vectoritcgroup.com>
2020-03-12 17:17:46 -0300

    Subindo projeto para o repositorio

---
+++ Controllers/TodoItemsController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TodoApiNew.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TodoItemsController : ControllerBase
+    {
+
+    }
+}
